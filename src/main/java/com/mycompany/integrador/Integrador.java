1195da40098e96bab7be61a6fe035cd3471b2001
ragevoid <riccardo.escorche@gmail.com>
2024-06-22 10:54:36 -0300

    Commit de Inicio

---
+++ src/main/java/com/mycompany/integrador/Integrador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.integrador;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class Integrador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
