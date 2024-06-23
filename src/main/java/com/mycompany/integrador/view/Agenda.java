5dbfea36e6f6256149700712ac0664a9120094c6
ragevoid <riccardo.escorche@gmail.com>
2024-06-22 23:08:32 -0300

    Update in the Agenda Frame using the JCalendar Library

--- src/main/java/com/mycompany/integrador/view/Agenda.java
+++ src/main/java/com/mycompany/integrador/view/Agenda.java
@@ -4,19 +4,28 @@
  */
 package com.mycompany.integrador.view;
 
+import com.toedter.calendar.JDayChooser;
+import com.toedter.calendar.JMonthChooser;
+import com.toedter.calendar.JYearChooser;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Calendar;
 /**
  *
  * @author Ricardo
  */
 public class Agenda extends javax.swing.JFrame {
-
+private Calendar calendar;
     /**
      * Creates new form Agenda
      */
     public Agenda() {
         initComponents();
+        initializeCalendar();
     }
-
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,22 +36,49 @@ public class Agenda extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jMonthChooser1 = new com.toedter.calendar.JMonthChooser();
+        jYearChooser1 = new com.toedter.calendar.JYearChooser();
         jDayChooser1 = new com.toedter.calendar.JDayChooser();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        jPanel1.add(jDayChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 360, 280));
+        jPanel1.add(jMonthChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, -1, -1));
+        jMonthChooser1.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                if ("month".equals(evt.getPropertyName())) {
+                    updateDayChooser();
+                }
+            }
+        });
+        jPanel1.add(jYearChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 30, -1, -1));
+        jYearChooser1.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                if ("year".equals(evt.getPropertyName())) {
+                    updateDayChooser();
+                }
+            }
+        });
+        jPanel1.add(jDayChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, 330, 310));
+        jDayChooser1.addPropertyChangeListener("day", new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent evt) {
+                if (evt.getPropertyName().equals("day")) {
+                    System.out.println("Selected Day: " + evt.getNewValue());
+                }
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 752, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -82,9 +118,25 @@ public class Agenda extends javax.swing.JFrame {
             }
         });
     }
+      private void initializeCalendar() {
+        calendar = Calendar.getInstance();
+        jMonthChooser1.setMonth(calendar.get(Calendar.MONTH));
+        jYearChooser1.setYear(calendar.get(Calendar.YEAR));
+        jDayChooser1.setDay(calendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    private void updateDayChooser() {
+        int selectedYear = jYearChooser1.getYear();
+        int selectedMonth = jMonthChooser1.getMonth();
+        calendar.set(selectedYear, selectedMonth, 1);
+        jDayChooser1.setMonth(selectedMonth);
+        jDayChooser1.setYear(selectedYear);
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.toedter.calendar.JDayChooser jDayChooser1;
+    private com.toedter.calendar.JMonthChooser jMonthChooser1;
     private javax.swing.JPanel jPanel1;
+    private com.toedter.calendar.JYearChooser jYearChooser1;
     // End of variables declaration//GEN-END:variables
 }
